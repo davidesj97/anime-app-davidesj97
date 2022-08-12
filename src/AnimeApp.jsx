c442651d543958f0ba4b6628e9712ded85c6908d
David Sánchez <davidesj97@gmail.com>
2022-08-12 11:17:24 -0500

    Created addCategory

--- src/AnimeApp.jsx
+++ src/AnimeApp.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const AnimeApp = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "Naruto"])
+  }
+
   return (
     <>
       {/* Titulo */}
       <h1>AnimeApp</h1>
 
       {/* Input busqueda */}
+      <AddCategory />
 
       {/* Listado */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ul>
+        {categories.map(category => (<li key={category}>{category}</li>))}
+      </ul>
     </>
   )
 }
