69a97625f3ec833e29cc29846538357f6fa371a6
David Sánchez <davidesj97@gmail.com>
2022-08-15 13:02:34 -0500

    API request created

--- src/components/AnimeGrid.jsx
+++ src/components/AnimeGrid.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import { getAnimes } from "../helpers/getAnimes";
 
 export const AnimeGrid = ({category}) => {
+
+  useEffect(() => {
+    getAnimes(category)
+  }, [])
+
+
   return (
     <>
       <h3>{ category }</h3>
---
+++ src/helpers/getAnimes.js
@@ -0,0 +1,18 @@
+export const getAnimes = async (category) => {
+  const BaseURL = `https://kitsu.io/api/edge/anime`
+  const URL = category ? `${BaseURL}?filter[text]=${category}` : BaseURL;
+  const res = await fetch(URL);
+  const { data } = await res.json();
+
+  const animes = data.map(anime => ({
+    id: anime.id,
+    title: anime.attributes.titles.en_jp,
+    image: anime.attributes.posterImage.original,
+    description: anime.attributes.description,
+    startDate: anime.attributes.startDate,
+    endDate: anime.attributes.endDate,
+  }))
+
+  console.log(animes);
+  return animes
+}
